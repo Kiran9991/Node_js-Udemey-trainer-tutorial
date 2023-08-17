57d76ac0204925142283a6321fccb174054d6dd8
Suraj <122242670+Kiran9991@users.noreply.github.com>
2023-08-17 17:10:26 +0530

    Created User Table with mongoose table

--- app.js
+++ app.js
@@ -13,19 +13,19 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findUserById("64dc8812e7dd13ffeaa10292")
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findUserById("64dc8812e7dd13ffeaa10292")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,8 +35,18 @@ app.use(errorController.get404);
 mongoose
 .connect('mongodb+srv://kiranagiwale123:WoL69WM4r3igFNyF@cluster0.6k3nc2k.mongodb.net/shop?retryWrites=true&w=majority')
 .then((result) => {
+  User.findOne().then(user => {
+    if(!user) {
+      const user = new User({
+        name: 'Kiran',
+        cart: {
+          items: []
+        }
+      });
+      user.save();
+    }
+  })
   app.listen(3000);
-  console.log('server is running on port=3000')
 }).catch(err => {
   console.log(err)
 })
