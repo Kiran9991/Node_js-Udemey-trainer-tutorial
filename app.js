fcfd50e1a1316559d2f4e7e55cd855d376d02809
Suraj <122242670+Kiran9991@users.noreply.github.com>
2023-08-17 16:37:31 +0530

    added mongoose methods

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
@@ -13,25 +13,30 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findUserById("64dc8812e7dd13ffeaa10292")
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findUserById("64dc8812e7dd13ffeaa10292")
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose
+.connect('mongodb+srv://kiranagiwale123:WoL69WM4r3igFNyF@cluster0.6k3nc2k.mongodb.net/shop?retryWrites=true&w=majority')
+.then((result) => {
   app.listen(3000);
+  console.log('server is running on port=3000')
+}).catch(err => {
+  console.log(err)
 })
